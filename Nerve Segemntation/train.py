acca381a7dcd1d8477414b223e4e8af22e1e55b0
Yashowardhan <yashowardhanshinde@gmail.com>
2022-10-30 02:05:55 +0530

    UNet

---
+++ Nerve Segemntation/train.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from unet import UNet
+
+# Jaccard loss, MSE
+# Adam Optimizer
+# Learning rate schedule
+# Checkpoint
+# Fixed Size = 512x512
---
+++ Nerve Segemntation/unet.py
@@ -0,0 +1,23 @@
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import BatchNormalization, Conv2D, Conv2DTranspose
+from tensorflow.keras.layers import MaxPooling2D, Dropout, Input, concatenate, UpSampling2D
+
+# UNet: Symmetric No Crops
+# Simple and Transpose Upsampling
+# BatchNorm and DropOut
+# Concat
+
+# ConvBlock
+def conv2d_block():
+    pass
+
+# Simple upsampling
+def upsample2d():
+    pass
+
+# Conv2DTranspose Upsample
+def upsample_conv2dtranspose():
+    pass
+
+def UNet():
+    pass
